4977ee60e1ebb4922dd4c48c193135b64bdf6910
tgo77 <tgo77@incon.kr>
2025-11-13 21:47:47 +0900

    assets

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,29 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 import "./global.css";
 
 
+
 export default function RootLayout() {
+
+  const [fontsLoaded ] = useFonts({
+    "Bubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
+    "Bubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+    "Bubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
+    "Bubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
+    "Bubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
+    "Bubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
+    
+  })
+
+  useEffect(()=>{
+    if(fontsLoaded){
+      SplashScreen.hideAsync();
+    }
+  },[fontsLoaded])
+
+  if(!fontsLoaded) return null;
+
   return <Stack />;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,33 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        rubik:["rubik-Regular", "sans-serif"],
+        "rubik-bold":["rubik-Bold", "sans-serif"],
+        "rubik-medium":["rubik-Medium", "sans-serif"],
+        "rubik-light":["rubik-Light", "sans-serif"],
+        "rubik-semibold":["rubik-SemiBold", "sans-serif"],
+        "rubik-extrabold":["rubik-ExtraBold", "sans-serif"],
+      },
+      colors:{
+        primary:{
+          100:"#0061ff0a",
+          200:"#0061ff1a",
+          300:"#0061ff" 
+        },
+        accent:{
+          100:"fbfbfd",
+
+        },
+        black:{
+          DEFAULT:"#000000",
+          100:"#8c8e98",
+          200:"#666876",
+          300:"#191d31"
+        }
+      }
+    },
   },
   plugins: [],
 }
